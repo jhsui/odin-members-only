7884e433823bf5320a2627266b9a0bea25004c61
jhsui <jiahansui1998@gmail.com>
2026-06-15 19:11:29 -0400

    update database connection to railway

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  database: process.env.DATABASE,
-  // password: process.env.PASSWORD,
-  port: process.env.PORT,
+  // host: process.env.HOST,
+  // user: process.env.USERNAME,
+  // database: process.env.DATABASE,
+  // // password: process.env.PASSWORD,
+  // port: process.env.PORT,
+
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
